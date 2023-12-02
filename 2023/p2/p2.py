63cc0870b9a71e285095cbe1c28d37e1be0780e7
Max Buck <maxbuckdeveloper@gmail.com>
2023-12-02 00:36:27 -0500

    p2 -- day 2 finished success

--- 2023/p2/p2.py
+++ 2023/p2/p2.py
@@ -1,32 +1,27 @@
-configuration = {
-    "red": 12,
-    "green": 13,
-    "blue": 14,
-}
-
-
-def is_game_possible(game_str):
-    """Return a tuple of (True/False, game_id); True if the game is possible, False otherwise."""
+def game_power(game_str):
+    """Return a tuple of (power, game_id)"""
 
     game_info, rounds_str = game_str.split(":")
     game_id = game_info.split("Game ")[1]
     game_id = game_id.strip()
     game_id = int(game_id)
+
+    min_possible = {"red": 0, "green": 0, "blue": 0}
     for round_str in rounds_str.split(";"):
         color_number_strs = round_str.split(",")
         for color_number_str in color_number_strs:
             number, color = color_number_str.strip().split(" ")
-            if configuration[color] < int(number):
-                return (False, game_id)
+            if min_possible[color] < int(number):
+                min_possible[color] = int(number)
 
-    return (True, game_id)
+    power = min_possible["red"] * min_possible["green"] * min_possible["blue"]
+    return (power, game_id)
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         total = 0
         for game_str in f:
-            possible, game_id = is_game_possible(game_str)
-            if possible:
-                total += game_id
+            power, game_id = game_power(game_str)
+            total += power
         print(total)
